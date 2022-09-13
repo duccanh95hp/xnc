cd5e31c74a1de40030e2d4ded16e5624397a9f64
98thanhnhu98 <97713475+98thanhnhu98@users.noreply.github.com>
2022-09-13 09:00:01 +0700

    Update CamXnc get API List Camxnc

--- src/main/java/com/example/xnc/reponsitory/CamXncRepository.java
+++ src/main/java/com/example/xnc/reponsitory/CamXncRepository.java
@@ -4,6 +4,9 @@ import com.example.xnc.model.CamXnc;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CamXncRepository extends JpaRepository<CamXnc, Long> {
+    List<CamXnc> findByGioiTinhAndHoTenAndNoiSinhAndQuocTich(int gioitinh,String hoten,String noisinh,String quoctich);
 }
